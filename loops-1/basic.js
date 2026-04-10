8a685a85403a298fb26e605f4b3b7c5f057bef7d
kumarmudur <kumarmudur10@gmail.com>
2026-04-10 07:56:10 +0530

    loops examples

---
+++ loops-1/basic.js
@@ -0,0 +1,38 @@
+// for loop
+// for (let i = 0; i <10; i++) {
+//     console.log(`Shiva ${i}`);
+// }
+
+// for (let i = 5; i > 0; i--) {
+//     console.log(`Shiva ${i}`);
+// }
+
+// function greet() {
+//     console.log('Hello!');
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     greet();
+// }
+
+let arr = [10, 6, 2, 5, 0, 8, 3];
+// let length = arr.length;
+// console.log(length);
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[10]);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// print even numbers
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) console.log(arr[i]);
+// }
+
+// print odd numbers
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] % 2 !== 0) console.log(arr[i]);
+// }
+
