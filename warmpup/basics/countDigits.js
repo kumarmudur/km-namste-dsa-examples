5a42ea9f15e4bdd0d793b9483839d585e66ce738
kumarmudur <kumarmudur10@gmail.com>
2026-04-14 16:41:41 +0530

    count digits examples

---
+++ warmpup/basics/countDigits.js
@@ -0,0 +1,11 @@
+function  countDigits(num) {
+    let count = 0;
+    while (num > 0) {
+        num = Math.floor(num / 10);
+        count++;
+    }
+    console.log('count', count);
+    return count;
+}
+
+console.log(countDigits(235))
