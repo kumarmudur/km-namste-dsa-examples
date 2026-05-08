4905be44acaa9719784d916f2e809c63d0f2d521
kumarmudur <kumarmudur10@gmail.com>
2026-05-08 09:19:23 +0530

    length of last word solution 3

--- strings/lastLength.js
+++ strings/lastLength.js
@@ -24,4 +24,17 @@ function lengthOfLastWord1(s) {
     return count;
 }
 
-console.log(lengthOfLastWord1('   Hello World    '));
+// solution 3
+function lengthOfLastWord2(s) {
+    let n = s.length - 1;
+    let count = 0;
+
+    while (n >= 0) {
+        if (s[n] !== ' ') ++count;
+        else if (count > 0) break;
+        --n;
+    }
+    return count;
+}
+
+console.log(lengthOfLastWord2('   Hello 123World    '));
