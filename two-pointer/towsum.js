49b3c71ceebd6c793ef86b1b2303ed897ea2056f
kumarmudur <kumarmudur10@gmail.com>
2026-06-18 07:13:30 -0400

    two sum examples

---
+++ two-pointer/towsum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function(nums, target) {
+    let n = nums.length;
+    let map = {};
+
+    for (let i = 0; i < n; i++) {
+        map[nums[i]] = i;
+    }
+
+    for (let i = 0; i < n; i++) {
+        let pairToFind = target - nums[i];
+        if (map[pairToFind] && map[pairToFind] !== i) {
+            return [i, map[pairToFind]];
+        }
+    }
+    return [-1, -1];
+};
